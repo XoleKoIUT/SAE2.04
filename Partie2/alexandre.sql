1ceb4a9cff9c110d5a982f2ea69121d6725a89fb
Alexandra Desplan <alexandradesplan@gmail.com>
2022-06-08 15:25:10 +0200

    Ajout de proc_c

--- Partie2/alexandre.sql
+++ Partie2/alexandre.sql
@@ -70,48 +70,86 @@ Select * From proc_b('Peter Pan'); FONCTIONNE*/
 
 /* Question C : Écrire une procédure qui prend en paramètre un nom d’éditeur et un nom d’auteur dessinateur et un nom d’auteur scénariste, et qui renvoie la liste des BD de ces auteurs éditées par l’éditeur choisi. Si l’éditeur n’a pas édité de BD de ces auteurs, ou qu’il n’existe pas de BD de ces deux auteurs, on devra générer le message suivant « l’éditeur % n’a pas édité de BD des auteurs % et %» où on remplacera les « % » par les noms correspondants. */
 
-/*
-DROP TYPE TypeEditeur cascade;
-DROP FUNCTION proc_c(nomEditeur Editeur.nomEditeur%TYPE, nomAutDess Auteur.nomAuteur%TYPE, nomAutScen Auteur.nomAuteur%TYPE);
 
-CREATE TYPE TypeEditeur AS ( TitreBD char,
-                             nomDess char,
-                             nomScen char);
+/*DROP FUNCTION proc_c (nom_editeur Editeur.nomEditeur%TYPE, nomAutDess Auteur.nomAuteur%TYPE, nomAutScen Auteur.nomAuteur%TYPE);
 
-CREATE OR REPLACE FUNCTION proc_c(nomEdit    Editeur.nomEditeur%TYPE,
-                                  nomAutDess Auteur.nomAuteur%TYPE,
-                                  nomAutScen Auteur.nomAuteur%TYPE)
-RETURNS setof TypeEditeur
+
+CREATE OR REPLACE FUNCTION proc_c (nom_editeur Editeur.nomEditeur%TYPE, nomAutDess Auteur.nomAuteur%TYPE, nomAutScen Auteur.nomAuteur%TYPE)
+returns setof BD
 AS $$
-DECLARE
-    tuple TypeEditeur%ROWTYPE;
+DECLARE 
+    num_Dessinateur Auteur.numAuteur%TYPE;
+    num_Scenariste  Auteur.numAuteur%TYPE;
 BEGIN
-FOR tuple IN 
-    Select B.titre
-    From   BD B Join Serie S
-           ON B.numSerie = S.numSerie
-           Join Editeur E ON
-           S.numEditeur = E.numEditeur
-    WHERE  nomEdit = E.nomEditeur 
-        
-    LOOP  
-        IF (NOT FOUND) THEN
-        RAISE NOTICE 'l editeur % n a pas edite de BD des auteurs % et %',
-                     nomEdit, nomAutDess, nomAutScen;
-        END IF;                       nomEdit, nomAutDess, nomAutScen;
-        
-        RETURN NEXT tuple;
-    END LOOP;
-    RETURN;
-END
-$$ LANGUAGE plpgsql;
 
-Select * From proc_c('Delcourt', 'Plessix', 'Plessix'); PAS FONCTIONNEL */
+        --on trouve le premier auteur
+        SELECT  numAuteur
+        INTO    num_Dessinateur
+        FROM    Auteur
+        WHERE   numAuteur = ( Select numAuteur
+                                         From   Auteur
+                                         Where  nomAuteur = nomAutDess);
+                                         
+        --on trouve le deuxième auteur
+        SELECT  numAuteur
+        INTO    num_Scenariste
+        FROM    Auteur
+        WHERE   numAuteur = ( Select numAuteur
+                                         From   Auteur
+                                         Where  nomAuteur = nomAutScen
+                                         );
+        RETURN QUERY
+        
+            SELECT  b.*
+            FROM    BD b NATURAL JOIN Editeur e
+            WHERE   numAuteurDessinateur = num_Dessinateur AND
+                    numAuteurScenariste  = num_Scenariste AND
+                    e.nomEditeur = nom_editeur;
+       
+            IF (NOT FOUND) THEN                        
+                RAISE NOTICE 'l’éditeur % n’a pas édité de BD des auteurs % et %', nom_editeur, nomAutDess, nomAutScen;
+            END IF;
+                
+END
+$$ LANGUAGE plpgsql;        
+
+Select * From proc_c('Dargaud', 'Uderzo', 'Goscinny'); 
+
+       isbn        |                       titre                       | prixactuel | numtome | numserie | numauteurdessinateur | numauteurscenariste 
+-------------------+---------------------------------------------------+------------+---------+----------+----------------------+---------------------
+ 978-2-2050-0096-2 | Astérix le gaulois                                |         12 |       1 |        2 |                   31 |                  14
+ 978-2-0121-0134-0 | La serpe d or                                     |         12 |       2 |        2 |                   31 |                  14
+ 978-2-0121-0135-7 | Astérix et les Goths                              |         12 |       3 |        2 |                   31 |                  14
+ 978-2-0121-0136-4 | Astérix Gladiateur                                |         12 |       4 |        2 |                   31 |                  14
+ 978-2-0121-0137-1 | Le tour de Gaule d Astérix                        |         12 |       5 |        2 |                   31 |                  14
+ 978-2-0121-0138-8 | Astérix et Cléopâtre                              |         12 |       6 |        2 |                   31 |                  14
+ 978-2-0121-0139-5 | Le combat des chefs                               |         12 |       7 |        2 |                   31 |                  14
+ 978-2-0121-0140-1 | Astérix chez les Bretons                          |         12 |       8 |        2 |                   31 |                  14
+ 978-2-0121-0141-8 | Astérix et les Normands                           |         12 |       9 |        2 |                   31 |                  14
+ 978-2-0121-0142-5 | Astérix Légionnaire                               |         12 |      10 |        2 |                   31 |                  14
+ 978-2-0121-0143-2 | Le bouclier Arverne                               |         12 |      11 |        2 |                   31 |                  14
+ 978-2-0121-0144-9 | Astérix aux jeux Olympiques                       |         12 |      12 |        2 |                   31 |                  14
+ 978-2-0121-0145-6 | Astérix et le chaudron                            |         12 |      13 |        2 |                   31 |                  14
+ 978-2-0121-0146-3 | Astérix en Hispanie                               |         12 |      14 |        2 |                   31 |                  14
+ 978-2-0121-0147-0 | La zizanie                                        |         12 |      15 |        2 |                   31 |                  14
+ 978-2-0121-0148-7 | Astérix chez les Helvètes                         |         12 |      16 |        2 |                   31 |                  14
+ 978-2-0121-0149-4 | Le domaine des dieux                              |         12 |      17 |        2 |                   31 |                  14
+ 978-2-0121-0150-0 | Les lauriers de César                             |         12 |      18 |        2 |                   31 |                  14
+ 978-2-0121-0151-7 | Le devin                                          |         12 |      19 |        2 |                   31 |                  14
+ 978-2-0121-0152-4 | Astérix en Corse                                  |         12 |      20 |        2 |                   31 |                  14
+ 978-2-0121-0153-1 | Le cadeau de César                                |         12 |      21 |        2 |                   31 |                  14
+ 978-2-0121-0154-8 | La grande traversée                               |         12 |      22 |        2 |                   31 |                  14
+ 978-2-0121-0155-5 | Obélix et compagnie                               |         12 |      23 |        2 |                   31 |                  14
+ 978-2-0121-0156-2 | Astérix chez les Belges                           |         12 |      24 |        2 |                   31 |                  14
+ 978-2-8649-7153-5 | Astérix et la rentrée gauloise                    |         12 |      32 |        2 |                   31 |                  14
+ 978-2-8649-7230-3 | L Anniversaire d Astérix & Obélix - Le livre d Or |         12 |      34 |        2 |                   31 |                  14
+(26 lignes)
+
 
 
 /* Question D : Créer une procédure stockée qui prend en paramètre le nom d’une série de BD et qui renvoie les clients ayants acheté tous les albums de la série (utiliser des boucles FOR et/ou des curseurs). Si aucun client ne répond à la requête alors on affichera un message d’avertissement ‘Aucun client n’a acheté tous les exemplaires de la série %’, en complétant le ‘ %’ par le nom de la série. */
 
-DROP FUNCTION proc_d(nom Serie.nomSerie%TYPE);
+/*DROP FUNCTION proc_d(nom Serie.nomSerie%TYPE);
 DROP TYPE TypeClient cascade;
 
 CREATE TYPE TypeClient AS ( title  text,
@@ -164,12 +202,12 @@ From   Client
 END
 $$ LANGUAGE plpgsql;
 
-Select Distinct * From proc_d('Dans un recoin de ce monde');
+Select Distinct * From proc_d('Dans un recoin de ce monde');*/
 
 /* Question E : Créer une procédure qui prend en paramètre un nombre nbBD de BD et une année donnée, et qui renvoie la liste des éditeurs ayant vendu au moins ce nombre de BD dans l’année en question. Si aucun éditeur ne répond à la requête, le signaler par un message approprié. */
-/*
 
-DROP FUNCTION proc_e(nbBD int, annee timestamp);
+
+/*DROP FUNCTION proc_e(nbBD int, annee timestamp);
 
 CREATE OR REPLACE FUNCTION proc_e(nbBD int, annee int)
 RETURNS setof Editeur
@@ -178,7 +216,7 @@ DECLARE
     un_type    Editeur%ROWTYPE;
     un_editeur Editeur.numEditeur%TYPE;
 BEGIN
-    FOR    numEditeur IN
+    FOR    un_editeur IN
     Select numEditeur
     From   Editeur
     
